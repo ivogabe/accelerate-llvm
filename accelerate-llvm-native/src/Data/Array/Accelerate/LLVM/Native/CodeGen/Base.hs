4158f734032a58929160efe3411338d30de6c199
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2025-09-02 14:07:06 +0200

    improve cache width

--- accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/CodeGen/Base.hs
+++ accelerate-llvm-native/src/Data/Array/Accelerate/LLVM/Native/CodeGen/Base.hs
@@ -55,13 +55,15 @@ class CalcValuesPerCacheLine t where
   valuesPerCacheLine :: t a -> Word64
 
 instance CalcValuesPerCacheLine PrimType where
-  valuesPerCacheLine tp = max 1 cacheWidth `div` fromIntegral (fst (primSizeAlignment tp))
+  valuesPerCacheLine tp = max 1 $ (cacheWidth + size - 1) `div` size    
+    where size = fromIntegral (fst (primSizeAlignment tp))
 
 instance CalcValuesPerCacheLine ScalarType where
   valuesPerCacheLine tp = valuesPerCacheLine $ ScalarPrimType tp
 
 instance CalcValuesPerCacheLine TypeR where
-  valuesPerCacheLine tp = max 1 cacheWidth `div` fromIntegral (bytesElt tp)
+  valuesPerCacheLine tp = max 1 $ (cacheWidth + size - 1) `div` size
+    where size = fromIntegral (bytesElt tp)
 
 -- The struct passed as argument to a call contains:
 --  * work_function: ptr
